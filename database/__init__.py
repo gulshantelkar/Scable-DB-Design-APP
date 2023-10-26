311babaa401f08d63ffedf09e7038a1b9df82a38
telkargulshan <gulshan.telkar@cogoport.com>
2023-10-26 12:41:47 +0530

    chanegs

--- database/__init__.py
+++ database/__init__.py
@@ -2,9 +2,9 @@ import databases
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
 database = PostgresqlExtDatabase(
-    'gulshantelkar',  # Your database name
-    user='gulshantelkar',  # Your PostgreSQL username
-    password='',  # Your PostgreSQL password
+    'gulshantelkar', 
+    user='gulshantelkar',  
+    password='',  
     host='localhost',
     port=5432
 )
